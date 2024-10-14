fe13c879221236bf9116632fca19cce6795cdbbf
Matthew <Matthew@DESKTOP-9E9PK4B>
2024-10-14 11:44:58 -0500

    Add project files.

---
+++ Destinations.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <map>
+#include <string>
+using namespace std;
+
+class Destinations
+{
+private:
+	map<string, string> destinations;
+
+public:
+	Destinations();
+	string find(string);
+};
---
+++ HackAssemblerF24.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "Destinations.h";
+using namespace std;
+
+int main()
+{
+	Destinations dstTbl;
+
+	cout << dstTbl.find("");
+}
+
+
+// build jump class
