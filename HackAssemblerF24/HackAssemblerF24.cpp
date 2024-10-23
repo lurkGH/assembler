9f158d99a2a181454fed4e6139e73e0eff105592
lurkoasistwitch@gmail.com <lurkoasistwitch@gmail.com>
2024-10-23 11:43:51 -0500

    feat: add initial implementation of assembly parser

--- HackAssemblerF24/HackAssemblerF24.cpp
+++ HackAssemblerF24/HackAssemblerF24.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "Destinations.h"
 #include "Jumps.h"
 #include "Computations.h"
 #include "Bin2Hex.h"
 using namespace std;
+
+void testInstructions(Destinations destInst, Jumps jmpInst, Computations cmpInst);
 
 int main(int argc, char* argv[])
 {
@@ -13,22 +16,10 @@ int main(int argc, char* argv[])
 	Computations compTbl;
 	Bin2Hex bin2HexTbl;
 	string inFileName, outFileName, inStr;
-
-	cout << "Testing Destination Instructions:" << endl;
-	cout << dstTbl.find("AMD") << endl;
-	cout << dstTbl.find("error") << endl << endl;
-
-	cout << "Testing Jump Instructions:" << endl;
-	cout << jmpTbl.find("JGT") << endl;
-	cout << jmpTbl.find("error") << endl << endl;
-
-	cout << "Testing Computation Instructions:" << endl;
-	cout << compTbl.find("!D") << endl;
-	cout << compTbl.find("error") << endl << endl;
-
-	cout << "Testing file input and output:" << endl;
-
-	// To add test files: right click HackAssemblerF24 in Solution Explorer, go to Properties, Debugging, and in Command Arguments put "test test.asm"
+	vector<string> instructions;
+	map<string, int> labels;
+	map<string, int>::iterator itLabels;
+	int lineCount = 0;
 
 	if (argc < 2) {
 		cout << "You must enter two parameters: input file and output file names" << endl;
@@ -40,7 +31,54 @@ int main(int argc, char* argv[])
 
 	ifstream inFile(inFileName);
 
-	while (getline(inFile, inStr, '\r')) {
+	// Pass 1
+	while (getline(inFile, inStr, '\n')) {
+		int commentLoc = inStr.find("//");
+		
+		// Removes comments
+		if (commentLoc != string::npos) {
+			inStr = inStr.substr(0, commentLoc);
+		}
+
+		// Removes empty lines
+		if (inStr.size() == 0) {
+			continue;
+		}
+
+		// Removes parentheses from loop targets
+		if (inStr.find("(") != string::npos && inStr.find(")") != string::npos) {
+			int leftLoc = inStr.find("(");
+			int rightLoc = inStr.find(")");
+			string label = inStr.substr(leftLoc + 1, rightLoc - (leftLoc + 1));
+			itLabels = labels.find(label);
+			if (itLabels == labels.end()) {
+				labels.insert(pair<string, int>(label, lineCount));
+			}
+			else {
+				cout << "Duplicate Label at line: " + lineCount << endl;
+			}
+			// DEBUG
+			//inStr = label;
+		}
+		else {
+			instructions.push_back(inStr);
+			lineCount++;
+		}
+
+		// DEBUG
 		cout << inStr << endl;
 	}
+}
+void testInstructions(Destinations destInst, Jumps jmpInst, Computations cmpInst) {
+	cout << "Testing Destination Instructions:" << endl;
+	cout << destInst.find("AMD") << endl;
+	cout << destInst.find("error") << endl << endl;
+
+	cout << "Testing Jump Instructions:" << endl;
+	cout << jmpInst.find("JGT") << endl;
+	cout << jmpInst.find("error") << endl << endl;
+
+	cout << "Testing Computation Instructions:" << endl;
+	cout << cmpInst.find("!D") << endl;
+	cout << cmpInst.find("error") << endl << endl;
 }
